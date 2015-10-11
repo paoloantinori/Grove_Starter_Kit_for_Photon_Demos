d0cc3469bbe03262e499b34209cc9df7a82a30fd
Paolo Antinori <sonicaaaa@gmail.com>
2015-10-11 11:20:05 +0200

    Added my notes no lesson 1

--- Example - 01 Singing A Song/Example01.ino
+++ Example - 01 Singing A Song/Example01.ino
@@ -1,31 +1,41 @@
-// -----------------------------------
-// Example - 01: Singing A Song
-// -----------------------------------
 #include "application.h"
+
+/*
+Basically we have connected a buzzer and a clicker.
+The main (infinite) loop checks when the clicker is currently pressed. If it is, invokes it's routing to play a song.
+*/
 
 // name the pins
 #define BUTTONPIN D2
 #define BUZZERPIN D1
 
-int melody[] = {1908,2551,2551,2273,2551,0,2024,1908};  // notes in the melody
-int noteDurations[] = {4,8,8,4,4,4,4,4 };               // note durations
+int melody[] = {1908, 2551, 2551, 2273, 2551, 0, 2024, 1908}; //notes in the melody
+int noteDurations[] = {4,8,8,4,4,4,4,4};                //note durations
 
-// This routine runs only once upon reset
-void setup()
-{
-  pinMode(BUTTONPIN, INPUT);                            // set user key pin as input
+void setup() {
+  pinMode(BUTTONPIN, INPUT);    // set user key pin as input
 }
 
-// This routine loops forever
-void loop()
-{
-  if(digitalRead(BUTTONPIN) == 1) {                     // if the button was pressed
-    for (int thisNote = 0; thisNote < 8; thisNote++) {  // ergodic all notes
-      int noteDuration = 1000/noteDurations[thisNote];  // calculate the note duration
-      tone(BUZZERPIN, melody[thisNote], noteDuration);  // let speaker sonds
-      int pauseBetweenNotes = noteDuration * 1.30;      // set a minimum time between notes
-      delay(pauseBetweenNotes);                         // delay for the while
-      noTone(BUZZERPIN);                                // stop the tone playing:
-    }
+void loop() {  
+  if(digitalRead(BUTTONPIN) == 1){
+      // if the button was pressed 
+      playSong();
+  }
+}
+
+void playSong(){
+  // loop over each defined note in the array
+  for( int thisNote = 0; thisNote < 8; thisNote++ ) {
+    // get node duration
+    int noteDuration = 1000 / noteDurations[thisNote];
+    // play note
+    tone(BUZZERPIN, melody[thisNote], noteDuration);
+    // set a minimum time between notes
+    int pauseBetweenNotes = noteDuration * 1.30;
+    //delay for the while
+    delay(pauseBetweenNotes);
+    // explicitely stop the tone playing
+    // not sure if it's really required
+    // noTone(BUZZERPIN);     
   }
 }
